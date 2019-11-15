90251f487ea08925e74259e2285683a8ae8ac6a2
sebainones <seba.inones@gmail.com>
2019-11-15 21:59:55 +0100

    Initial commit

---
+++ Reminders.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Reminders.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
